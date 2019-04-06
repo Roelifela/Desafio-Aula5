76aaa46d900f411ec56672b847674d0e75b64579
unknown <Digital@DH-A8-01.dhbrasil.digitalhouse.com>
2019-04-05 22:13:16 -0300

    Initial commit

---
+++ src/br/com/digitalhouse/Main.java
@@ -0,0 +1,52 @@
+package br.com.digitalhouse;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Filho filhoUm = new Filho("Maria", "Jorge", "Jose");
+        Filho filhoDois = new Filho("Maria", "Vitor", "Ana");
+
+
+        System.out.println(saoMeioIrmaos(filhoUm, filhoDois));
+
+
+    }
+
+
+    public static String maeDe(Filho novoFilho) {
+
+        return novoFilho.getMae();
+
+    }
+
+
+    public static String paiDe(Filho novoFilho) {
+
+        return novoFilho.getPai();
+    }
+
+
+    public static boolean mesmaMae(Filho filhoUm, Filho filhodois) {
+        return maeDe(filhoUm) == maeDe(filhodois);
+    }
+
+    public static boolean mesmoPai(Filho filhoUm, Filho filhodois) {
+//        return maeDe(filhoUm) == maeDe(filhodois);
+
+        if (paiDe(filhoUm) == paiDe(filhodois)) {
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean saoMeioIrmaos(Filho filhoUm, Filho filhoDois) {
+
+        return mesmaMae(filhoUm, filhoDois) && !mesmoPai(filhoUm, filhoDois)
+                || !mesmaMae(filhoUm, filhoDois) && mesmoPai(filhoUm, filhoDois);
+
+    }
+
+}
